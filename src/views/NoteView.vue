6b4859ce5c4c9cf33f68dd7e73beb68280de4b67
{waltz3} <{mowaltz1703@gmail.com}>
2025-06-21 11:50:13 +0900

    Note型

--- src/views/NoteView.vue
+++ src/views/NoteView.vue
@@ -1,5 +1,38 @@
 <template>
   <h1>Note View</h1>
+  <div v-if="note">
+    <div class="note-content">
+      <h2>Revision: {{ note.revision }}</h2>
+      <p><strong>Channel:</strong> {{ note.channel }}</p>
+      <p><strong>Permission:</strong> {{ note.permission }}</p>
+      <div class="note-body" v-html="note.body"></div>
+
+    </div>
+
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import{ref, onMounted} from 'vue';
+import { useRoute } from 'vue-router';
+
+interface Note {
+  revision: string;,
+  channel: string;,
+  permission: string;,
+  body: string;
+}
+
+const note = ref<Note>({
+  revision: '',
+  channel: '',
+  permission: '',
+  body: ''
+});
+
+
+
+
+
+</script>
+
