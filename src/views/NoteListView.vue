1b25117b6bc31115c5106415e76beb8762fa2322
SAH123 <shota.himeji@m.ieice.org>
2025-06-21 15:19:13 +0900

    修正

--- src/views/NoteListView.vue
+++ src/views/NoteListView.vue
@@ -4,34 +4,33 @@
   <div class="box">
   <p>チャンネル:</p>
   <input type="search" id="channel" name="search">
-  <button onclick="channel()" ><img src="../assets/検索_mock.png" width="25px" height="25px"/></button>
+  <button @click="channel()" ><img src="../assets/search_mock.png" width="25px" line-height="25px"/></button>
   <input type="checkbox" checked>
   <p>子チャンネルを含む</p>
   </div>
   <div class="box">
   <p>キーワード:</p>
   <input type="search" id="keywords">
-  <button onclick="keywords()"><img src="../assets/検索_mock.png" width="25" height="25"/></button>
+  <button @click="keywords()"><img src="../assets/search_mock.png" width="25" line-height="25"/></button>
   </div>
-  <button onclick="setting()" id="setting">設定</button>
-  <button onclick="newnote()" id="newnote">新規ノートを追加</button>
+  <button @click="setting()" id="setting">設定</button>
+  <button @click="newnote()" id="newnote">新規ノートを追加</button>
   </div><br>
 
   <div class="sort">
-  <button onclick="datesort()" id="datesort">日時順でソート</button>
-  <button onclick="titlesort()" id="titlesort">タイトル順でソート</button>
+  <button @click="datesort()" id="datesort">日時順でソート</button>
+  <button @click="titlesort()" id="titlesort">タイトル順でソート</button>
   </div>
 </template>
 
 <script setup lang="ts">
 
 
-
 </script>
 
 <style>  
   .search {
-    height: auto;
+    line-height: auto;
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -39,7 +38,7 @@
     gap: 20px
   }
   .box {
-    height: 29px;
+    line-height: 29px;
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -49,7 +48,7 @@
 
   #newnote {
     background: #ABD9AE;
-    height: 48px;
+    line-height: 48px;
     border-width: 1px ;
     border-color: #3F8D44;
     align-items: center;
@@ -58,20 +57,25 @@
 
   }
   #setting {
-    height: 29px;
-    align-items: center;
     line-height: 29px;
+    align-items: center;
+    line-line-height: 29px;
    text-align: center;
   }
 
 
   input[type="search"] {
-    height: 29px;
+    line-height: 29px;
     align-items: center;
   }
-
+  input[type=checkbox] {
+    line-height: 29px;
+    width: 29px;
+    margin: auto;
+    transform: scale(1.6);
+  }
   button {
-    height: 29px;
+    line-height: 29px;
     cursor: pointer;
     background-color: white;
     border-width: 1px;
@@ -85,20 +89,16 @@
     flex-direction: row;
     align-items: center;
     white-space: nowrap;
-    gap: 24px;
+    gap: 10px;
     font-size: 29px;
-    margin-left:150px;
+    justify-content: center;
   }
+
   #datesort, #titlesort {
-    height: 47px;
+    line-line-height: 47px;
     border-width: 1px ;
   }
-  input[type=checkbox] {
-    height: 29px;
-    width: 29px;
-    margin: 0 8px;
-    transform: scale(0.8);
-  }
+
 
   
 </style>
