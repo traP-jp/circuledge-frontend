e2d1b0e9dad520ed3ee191df1bb25e12e78becbd
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-06-22 00:54:31 +0900

    ヘッダーを実装・App.vueのスタイルを調整

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,36 @@ import Header from './components/common/Header.vue';
 </script>
 
 <template>
-  <Header />
-  <main class="container">
-    <RouterView />
-  </main>
+  <div class="app">
+    <Header />
+    <main class="main-content">
+      <RouterView />
+    </main>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+.main-content {
+  flex: 1;
+  overflow: auto;
+}
+</style>
+
+<!-- 次のクラスは、#app に適用させるため scoped にしない -->
+<style>
+html,
+body {
+  height: 100%;
+}
+
+#app {
+  height: 100dvh;
+}
+</style>
