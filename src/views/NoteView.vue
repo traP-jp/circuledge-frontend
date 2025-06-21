da8c3e4921dcec11937c39b8fd1dde9ff76d362b
{waltz3} <{mowaltz1703@gmail.com}>
2025-06-21 12:06:41 +0900

    要素を羅列した

--- src/views/NoteView.vue
+++ src/views/NoteView.vue
@@ -1,18 +1,26 @@
 <template>
   <h1>Note View</h1>
-  <div v-if="note">
-    <div class="note-content">
-      <h2>Revision: {{ note.revision }}</h2>
-      <p><strong>Channel:</strong> {{ note.channel }}</p>
-      <p><strong>Permission:</strong> {{ note.permission }}</p>
-      <div class="note-body" v-html="note.body"></div>
-
+  <div class="note-view">
+    <div class="note-channel">channel:</div>
+    <div class="note-tags">tags:</div>
+    <div class="note-action">
+      <button class="note-action-button">編集</button>
+      <button class="note-action-button">ホームへ戻る</button>
     </div>
+    <h1 class="note-title">タイトル</h1>
+    <div class="note-body">
+      <p>ここにノートの内容が表示されます。</p>
+      <p>ノートの内容は、ユーザーが入力したテキストや画像などを含むことができます。</p>
+      <p>このセクションでは、ノートの詳細な情報を表示します。</p>
+    </div>
+
+
 
   </div>
 </template>
 
 <script setup lang="ts">
+/*
 import{ref, onMounted} from 'vue';
 import { useRoute } from 'vue-router';
 
@@ -29,6 +37,8 @@ const note = ref<Note>({
   permission: '',
   body: ''
 });
+*/
+
 
 
 
